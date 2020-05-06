b8d9ec33f4cb5b88fe081a90711c5c2986ef4e83
LittleSoap <32540199+LittleSoap@users.noreply.github.com>
2020-05-06 16:14:13 +0200

    train mlp batch 1

--- scripts/training_script_matthieu.py
+++ scripts/training_script_matthieu.py
@@ -1,3 +1,5 @@
+from tensorflow_core.python.keras.backend import clear_session
+
 from ModelUtils.Models.LinearModel import *
 from ModelUtils.Models.MLP import *
 from ModelUtils.Models.structurer.LinearStructurer import *
@@ -9,33 +11,52 @@ from tensorflow.compat.v1 import set_random_seed
 import tensorflow as tf
 
 if __name__ == '__main__':
-    images_size = 16
+    # images_size = 16
     my_batch = 1
-    nb_train_batches = 1
-    nb_test_batches = 1
+    # nb_train_batches = 1
+    # nb_test_batches = 1
     validation_split = 0.2
-    epochs = 500
+    # epochs = 500
+    #
+    # epochs = 2000
+    # for i in range(3):
+    #     # #set_random_seed(10)
+    #     # linear_model_struct = LinearStructurer()
+    #     # # linear_model_struct.nb_classes = 572
+    #     # linear_model_struct.input_shape = (images_size, images_size, 3)
+    #     # #model = create_linear_model(linear_model_struct)
+    #     # description = getLinStructAsString(linear_model_struct)
+    #
+    #     # model_path = '{}{}\\{}\\{}\\'.format(TRAINED_MODEL_DIR, "model_lineaire", images_size, "batch_0")
+    #     # print(model_path)
+    #     # model_name = "0_16_linear_model_0_11.h5"
+    #     # model = load_saved_model(model_path, model_name)
+    #
+    #     #fit saved model
+    #     model_name_root = train_model_batch(LINEAR_MODEL, model, description, my_batch, images_size, nb_train_batches, nb_test_batches, validation_split, epochs)
+    #     test_model_batch(LINEAR_MODEL, model, model_name_root, my_batch, images_size, nb_test_batches)
+    #     epochs = epochs * 2
+    #
+    # #model_juba = tf.keras.models.load_model('../trained_model/model_lineaire/16/batch_0/0_16_linear_model_0_11.h5')
+    # #model_juba.summary()
 
-    epochs = 2000
+    images_size = 64
+    nb_train_batches = 2
+    nb_test_batches = 1
+    epochs = 1000
+    batch_size = 2048
     for i in range(3):
-        #set_random_seed(10)
-        linear_model_struct = LinearStructurer()
-        # linear_model_struct.nb_classes = 572
-        linear_model_struct.input_shape = (images_size, images_size, 3)
-        #model = create_linear_model(linear_model_struct)
-        description = getLinStructAsString(linear_model_struct)
-
-        model_path = '{}{}\\{}\\{}\\'.format(TRAINED_MODEL_DIR, "model_lineaire", images_size, "batch_0")
-        print(model_path)
-        model_name = "0_16_linear_model_0_11.h5"
-        model = load_saved_model(model_path, model_name)
-
-        #fit saved model
-        model_name_root = train_model_batch(LINEAR_MODEL, model, description, my_batch, images_size, nb_train_batches, nb_test_batches, validation_split, epochs)
-        test_model_batch(LINEAR_MODEL, model, model_name_root, my_batch, images_size, nb_test_batches)
+        mlp_struct = MlpStructurer()
+        mlp_struct.nb_hidden_layers = 5
+        mlp_struct.nb_classes = 572
+        mlp_struct.input_shape = (images_size, images_size, 3)
+        mlp_struct.layers_size = [32, 32, 32, 32, 32]
+        model = create_custom_mlp(mlp_struct)
+        description = getMlpStructAsString(mlp_struct)
+        model_name_root = train_model_batch(MLP, model, description, my_batch, images_size, nb_train_batches,
+                                            nb_test_batches, validation_split, epochs, batch_size=batch_size)
+        test_model_batch(MLP, model, model_name_root, my_batch, images_size, nb_test_batches)
         epochs = epochs * 2
-
-    #model_juba = tf.keras.models.load_model('../trained_model/model_lineaire/16/batch_0/0_16_linear_model_0_11.h5')
-    #model_juba.summary()
+        clear_session()
 
 
